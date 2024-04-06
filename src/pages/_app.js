78d2ea4d7737c9567bdffd3dda978bf545e907df
real-folkblues <marenicolor@gmail.com>
2024-04-06 19:17:19 -0400

    upgraded to nextjs 13, trying to work out google font issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "@/styles/globals.scss";
 // import { Workbench } from 'next/font/google';
+
 import { Montserrat } from 'next/font/google';
 import { useEffect } from 'react';
 
-
-
-
-
 const montserrat = Montserrat ({subsets: ['latin']});
-
 
 
 export default function App({ Component, pageProps }) {
