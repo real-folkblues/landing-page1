c80c2cf4e2166c69f6d79d7fd375860f4ea4533d
Real-folkblues <marenicolor@gmail.com>
2024-06-15 19:54:57 -0400

    Added file for Navbar, created Navbar class which extends component

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { render } from 'sass';
+
+class NavBar extends Component {
+    render() {
+        return (
+
+        <div id="section_header">
+  <h1>Main Page Title</h1>
+  <img src="mdn-logo-sm.png" alt="MDN logo" />
+            </div>
+
+    )}
+}
+
+export default NavBar;
